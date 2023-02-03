e6104a06527c3f9684afad1bbd0ddb80d9ecce63
DevenBL <62316193+DevenBL@users.noreply.github.com>
2023-02-03 18:51:33 -0500

    an unreliable setup script

---
+++ setup.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+python3 -m venv .
+
+source ./bin/activate
+
+python -m pip install pip -U pip
+python -m pip install -r requirements.txt
+
+echo "run the .py scripts to start using the whisper mic"
+echo "to leave the python virtual enviroment typer "deactivate"
