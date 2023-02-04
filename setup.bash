341e52fc9b3e3243a15203592fa3e25012183525
DevenBL <62316193+DevenBL@users.noreply.github.com>
2023-02-04 11:13:39 -0500

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -7,4 +7,4 @@ python -m pip install pip -U pip
 python -m pip install -r requirements.txt
 
 echo "run the .py scripts to start using the whisper mic"
-echo "to leave the python virtual enviroment typer "deactivate"
+echo "to leave the python virtual enviroment typer deactivate"
