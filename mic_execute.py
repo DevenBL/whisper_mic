801e62212028f9731e65923107561ee3848b6df3
DevenBL <62316193+DevenBL@users.noreply.github.com>
2023-02-04 05:40:40 +0000

    Added comments and reminders
    too much caveman code needs fixing but it works

--- mic_execute.py
+++ mic_execute.py
@@ -15,7 +15,7 @@ from os.path import isfile, join
 @click.command()
 @click.option("--model", default="base", help="Model to use", type=click.Choice(["tiny","base", "small","medium","large"]))
 @click.option("--scriptpath", help="runs scripts in scriptpath directories based on keywords", type=click.Path())
-# for when i figure out how to make this execute from multiple directories
+# for when i figure out how to make this search and execute from multiple directories
 # @click.option("--scriptpath", help="runs scripts in scriptpath directories based on keywords", multiple=True, type=click.Path())
 @click.option("--device", default=("cuda" if torch.cuda.is_available() else "cpu"), help="Device to use", type=click.Choice(["cpu","cuda"]))
 @click.option("--english", default=False, help="Whether to use English model",is_flag=True, type=bool)
@@ -39,11 +39,11 @@ def main(model, english,verbose, energy, pause,dynamic_energy,save_file,device,s
     
     # i could add more file extensions here but honestly add them yourself
     acceptablescripttypes = ('.bash','.py')
-# for when i figure out how to make this execute from multiple directories
-#    for dirs in scriptpath:
-    dirs = scriptpath
 
-    keywordlist = [scriptfile for scriptfile in listdir(dirs) if isfile(join(dirs, scriptfile)) and scriptfile.endswith(acceptablescripttypes) ]
+    # Honestly i really just caveman'ed this section together all the way to os.system(exec) and it should really be rewritten, reformatted, optimized and multithreaded but at least it works even if it has some edge cases 
+    # todo Edgecases: for example if the script is named 'say something.bash' or 'you said.bash' im pretty sure it would just execute the script every time since that's a part of the non transctiption output that i didnt bother excluding
+
+    keywordlist = [scriptfile for scriptfile in listdir(scriptpath) if isfile(join(scriptpath, scriptfile)) and scriptfile.endswith(acceptablescripttypes) ]
     print("Keyword list :" + str(keywordlist))
     
     while True:
@@ -53,7 +53,7 @@ def main(model, english,verbose, energy, pause,dynamic_energy,save_file,device,s
              for skrtypes in acceptablescripttypes:
                   if keywords.endswith(skrtypes) and keywords.removesuffix(skrtypes).upper() in model_output.upper():
              	      print("keyword recognized: " + str(keywords.removesuffix(skrtypes)))
-             	      os.system('exec ' + '"' + dirs + keywords + '" &')
+             	      os.system('exec ' + '"' + scriptpath + keywords + '" &')
 
 def record_audio(audio_queue, energy, pause, dynamic_energy, save_file, temp_dir):
     #load the speech recognizer and set the initial energy threshold and pause threshold
